bb686434b5c9e98fd85f308cc918dcabd8b043ff
Talha <mdtalha2008@gmail.com>
2024-08-28 00:59:17 +0600

    footer update

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="footer footer-center bg-sky-950 text-base-content rounded p-10">
-        <nav className="grid grid-flow-col gap-4">
+        <nav className="grid grid-flow-col gap-4 text-white">
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
@@ -26,7 +26,7 @@ const Footer = () => {
           </div>
         </nav>
         <aside>
-          <p>
+          <p className="text-white">
             Copyright © {new Date().getFullYear()} - All right reserved by ACME
             Industries Ltd
           </p>
